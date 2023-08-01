e52a7a2bcccdbacec0c92d4ed2ce3c2d7e052f5d
Thanh Do <ducthanhdo.developer@gmail.com>
2023-08-01 23:49:48 +0200

    remove backend_node folder, ajust the backend folder name

--- backend_node/routes/events-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const eventsController = require('../controllers/events-controller');
-// eslint-disable-next-line new-cap
-const router = express.Router();
-
-router.get('/', eventsController.getEvents);
-router.get('/:dynasty', eventsController.getEventsByDynasty);
-module.exports = router;
